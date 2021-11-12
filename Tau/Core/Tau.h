90e9242732933f7b03cb3433b522cdf8699d925d
Kazuma Ohura <k.ohura0303@gmail.com>
2021-11-12 16:57:41 +0900

    🚧 vcxprojファイルの更新

---
+++ Tau/Core/Tau.h
@@ -0,0 +1,19 @@
+#ifndef TAU_H_
+
+#include <memory>
+#include <stdint.h>
+#include <windows.h>
+
+namespace Tau
+{
+class Tau
+{
+public:
+	static int16_t Init();
+	static int16_t Release();
+
+private:
+};
+}	 // namespace Tau
+
+#endif	// TAU_H_
